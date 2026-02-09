b6ab56485dd1c595472e3444886c8f7cf579642c
tgede46 <gedeonkpara@gmail.com>
2026-02-09 02:05:12 +0000

    first commit

---
+++ src/main/java/com/bijouxshop/bijouxshop/BijouxshopApplication.java
@@ -0,0 +1,13 @@
+package com.bijouxshop.bijouxshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BijouxshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BijouxshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bijouxshop/bijouxshop/BijouxshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bijouxshop.bijouxshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BijouxshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
